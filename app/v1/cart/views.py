a96935dc4900dccf98dac23a00bc86c733011664
Rahul-1991 <rahul.srivastava@students.iiit.ac.in>
2016-07-19 20:01:09 +0530

    Integrating swagger with addToCart API

--- app/v1/cart/views.py
+++ app/v1/cart/views.py
@@ -97,6 +97,103 @@ class AddToMyCart(View):
     }
 
     def dispatch_request(self):
+        #@process_params(param_config=self.param_config['with_auth'], header_config=header_config['with_auth'])
+        #@authorization
+        def add_user_cart():
+            print "inside auth"
+            return render_success_response(self.response_data, 200)
+
+        #@process_params(param_config=self.param_config['without_auth'], header_config=header_config['without_auth'])
+        def add_guest_cart():
+            print "inside guest"
+            return render_success_response(self.response_data, 200)
+
+        def perform():
+            if request.headers.get("Authorization"):
+                response = add_user_cart()
+            else:
+                response = add_guest_cart()
+            print response
+            return response
+
+        return perform()
+
+
+class MyCartDetails(View):
+    methods = ['POST']
+    param_config = {
+        'with_auth': {
+            'type': 'object',
+            'properties': {
+                'customerId': {'type': 'string'},
+                'productId': {'type': 'string'},
+                'qty': {'type': 'number'}
+            },
+            'required': ['customerId', 'productId', 'qty']
+        },
+        'without_auth': {
+            'type': 'object',
+            'properties': {
+                'productId': {'type': 'string'},
+                'qty': {'type': 'number'}
+            },
+            'required': ['productId', 'qty']
+        }
+    }
+
+    response_data = {
+        "s": 1,
+        "m": "Item sucessfully added to cart!",
+        "d": {
+            "products": [
+                {
+                    "regularPrice": 399,
+                    "discountedPrice": 300,
+                    "discountPercentage": 20,
+                    "vendorDetails": {
+                        "vendorId": "1",
+                        "vendorCod": 1,
+                        "vendorName": "Dola's Creation",
+                        "vendorPincode": "400078"
+                    },
+                    "discountPercentage": 5,
+                    "productName": "Work Intel Anklet",
+                    "qty": 2,
+                    "sellerNote": None,
+                    "imgUrl": "/C/V/CV-MVISH51837745550-saree-Craftsvilla_1.jpg",
+                    "productId": "1022"
+                },
+                {
+                    "regularPrice": 699,
+                    "discountedPrice": 400,
+                    "discountPercentage": 20,
+                    "vendorDetails": {
+                        "vendorId": "1",
+                        "vendorCod": 1,
+                        "vendorName": "Dola's Creation",
+                        "vendorPincode": "400078"
+                    },
+                    "discountPercentage": 0,
+                    "productName": "Blue bage",
+                    "qty": 1,
+                    "sellerNote": None,
+                    "imgUrl": "/C/V/CV-MVISH51837745550-bags-Craftsvilla_1.jpg",
+                    "productId": "10282"
+                }
+            ],
+            "orderSummary": {
+                "totalPayable": 799,
+                "couponDiscount": 0,
+                "shippingCost": 0,
+                "discount": 0,
+                "total": 799,
+                "subTotal": 799,
+                "vat": 0
+            }
+        }
+    }
+
+    def dispatch_request(self):
         @process_params(param_config=self.param_config['with_auth'], header_config=header_config['with_auth'])
         @authorization
         def add_user_cart(params, header_params, auth):
